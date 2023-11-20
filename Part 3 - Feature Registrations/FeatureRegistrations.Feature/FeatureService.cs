1c586c6c421c5caa8dabba271935539102f8a113
Simon Jefferies <simon.jefferies@inseinc.com>
2023-11-20 11:58:43 +0000

    Add feature registrations example.

---
+++ Part 3 - Feature Registrations/FeatureRegistrations.Feature/FeatureService.cs
@@ -0,0 +1,8 @@
+﻿using FeatureRegistrations.Abstractions;
+
+namespace FeatureRegistrations.Feature;
+
+public class FeatureService : IFeatureService
+{
+    public Guid Id { get; } = Guid.NewGuid();
+}
